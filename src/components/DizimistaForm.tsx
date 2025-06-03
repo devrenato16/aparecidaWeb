bf0dbd35d9b747dc83517b42c4bf4be0dcf205ea
renatoads16 <dev.renatobarros@gmail.com>
2025-06-03 18:04:44 -0300

    dizimista

--- src/components/DizimistaForm.tsx
+++ src/components/DizimistaForm.tsx
@@ -14,16 +14,14 @@ export const DizimistaForm = ({
   errors,
   control,
 }: DizimistaFormProps) => {
-  const motherName = useWatch({ name: "motherName", control });
+  const fullName = useWatch({ name: "fullName", control });
   const currentDate = format(new Date(), "dd/MM/yyyy");
-
-  const specialNeeds = useWatch({ name: "specialNeeds", control });
 
   return (
     <>
       <FormField
         label="Nome completo:"
-        name="name"
+        name="fullName"
         register={register}
         error={errors.name}
         options={{ required: "O nome é obrigatório" }}
@@ -66,21 +64,12 @@ export const DizimistaForm = ({
       />
 
       <FormField
-        label="Telefone do Responsável:"
+        label="Telefone:"
         name="phone"
         register={register}
         error={errors.phone}
         options={{ required: "O telefone é obrigatório" }}
         placeholder="(00) 00000-0000"
-      />
-
-      <FormField
-        label="Nome do Pai:"
-        name="fatherName"
-        register={register}
-        error={errors.fatherName}
-        options={{ required: "Este campo é obrigatório" }}
-        className="md:col-span-2"
       />
 
       <FormField
@@ -101,90 +90,6 @@ export const DizimistaForm = ({
         options={{ required: "Este campo é obrigatório" }}
       />
 
-      <FormField
-        label="Série Escolar:"
-        name="schooling"
-        register={register}
-        error={errors.schooling}
-        options={{ required: "Este campo é obrigatório" }}
-      />
-
-      {/* Batizado */}
-      <FormField
-        label="A Criança é batizada?"
-        name="isBaptized"
-        register={register}
-        error={errors.isBaptized}
-        options={{ required: "Campo obrigatório" }}
-      >
-        <div className="flex gap-4">
-          <label>
-            <input type="radio" value="sim" {...register("isBaptized")} /> Sim
-          </label>
-          <label>
-            <input type="radio" value="nao" {...register("isBaptized")} /> Não
-          </label>
-        </div>
-      </FormField>
-
-      {/* Necessidade especial */}
-      <FormField
-        label="Possui necessidade especial?"
-        name="specialNeeds"
-        register={register}
-        error={errors.specialNeeds}
-        options={{ required: "Campo obrigatório" }}
-      >
-        <div className="flex gap-4">
-          <label>
-            <input type="radio" value="sim" {...register("specialNeeds")} /> Sim
-          </label>
-          <label>
-            <input type="radio" value="nao" {...register("specialNeeds")} /> Não
-          </label>
-        </div>
-      </FormField>
-
-      {/* Campo condicional */}
-      {specialNeeds === "sim" && (
-        <FormField
-          label="Se sim, qual?"
-          name="specialNeedsDetails"
-          register={register}
-          error={errors.specialNeedsDetails}
-          placeholder="Qual?"
-          className="md:col-span-2"
-          options={{
-            validate: (value) =>
-              specialNeeds === "sim" && !value
-                ? "Este campo é obrigatório quando a resposta é 'Sim'"
-                : true,
-          }}
-        />
-      )}
-
-      {/* Horário de participação */}
-      <FormSelect
-        label="Qual horário a criança poderá participar?"
-        name="availableDay"
-        register={register}
-        error={errors.availableDay}
-        options={[
-          { value: "matriz_7h30", label: "Matriz de Aparecida, 7h30 - 9h00" },
-          { value: "matriz_11h30", label: "Matriz de Aparecida, 9h30 - 11h00" },
-          {
-            value: "matriz_13h00",
-            label: "Matriz de Aparecida, 13h00 - 15h00",
-          },
-          { value: "cap_8h00", label: "Capela São Sebastião, 8h00 - 10h00" },
-          {
-            value: "cap_16h00",
-            label: "Capela São Pedro e São Paulo, 16h00 - 18h00",
-          },
-        ]}
-        className="md:col-span-2"
-      />
-
       {/* Termo de Compromisso */}
       <div className="mb-4 md:col-span-2">
         <h3 className="text-lg font-bold text-center mb-6 whitespace-nowrap">
@@ -193,12 +98,11 @@ export const DizimistaForm = ({
         <p className="text-left">
           <span className="font-normal">Eu, </span>
           <span className="font-bold inline-block w-full sm:max-w-md p-1 rounded">
-            {motherName || ""}
+            {fullName || ""}
           </span>
-          , a observar e motivar a participação do meu filho (a) nos ENCONTROS
-          DE FORMAÇÕES DA CATEQUESE, NECESSÁRIOS PARA O MESMO(A) RECEBER O
-          SACRAMENTO DA PRIMEIRA EUCARISTIA, e estou consciente que, faltando a
-          esses COMPROMISSOS, NÃO poderá receber o SACRAMENTO.
+          , declaro meu compromisso em contribuir com a vida da Igreja por meio
+          do pagamento do dízimo. Assumo essa responsabilidade com fé e
+          generosidade, apoiando a obra de Deus em nossa comunidade.
         </p>
         {errors.termName && (
           <p className="text-red-500 text-sm mt-1">{errors.termName.message}</p>
